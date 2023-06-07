20523b53f76e08978107ce5d9de37e8b671a7044
Pavlo Tytarchuk <ptyta@softserveinc.com>
2023-06-07 15:30:01 +0200

    DAT-15075. added possibility to specify changelog format you want to run in HarnessNoSqlCompatibility test

--- src/test/groovy/liquibase/harness/compatibility/foundational/CompatibilityTestHelper.groovy
+++ src/test/groovy/liquibase/harness/compatibility/foundational/CompatibilityTestHelper.groovy
@@ -15,21 +15,33 @@ class CompatibilityTestHelper {
     final static List supportedChangeLogFormats = ['xml', 'json', 'yml', 'yaml'].asImmutable()
 
     static List<TestInput> buildTestInput(String changelogPathSpecification) {
-        String commandLineInputFormat = System.getProperty("inputFormat")
+        String specificChangelogPath = baseChangelogPath + changelogPathSpecification
+        String commandLineInputFormats = System.getProperty("inputFormat")
         String commandLineChangeObjects = System.getProperty("changeObjects")
+
+        List inputFormatList = Collections.emptyList()
         List commandLineChangeObjectList = Collections.emptyList()
+
+        if (commandLineInputFormats) {
+            TestConfig.instance.inputFormat = commandLineInputFormats
+        }
+
+        inputFormatList = Arrays.asList(TestConfig.instance.inputFormat.contains(",")
+                ? TestConfig.instance.inputFormat.split(",")
+                : TestConfig.instance.inputFormat)
+
+        for(String inputFormat: inputFormatList){
+            if (!supportedChangeLogFormats.contains(inputFormat)) {
+                throw new IllegalArgumentException(inputFormat + " inputFormat is not supported")
+            }
+        }
+
         if (commandLineChangeObjects) {
             commandLineChangeObjectList = Arrays.asList(commandLineChangeObjects.contains(",")
                     ? commandLineChangeObjects.split(",")
                     : commandLineChangeObjects)
         }
-        String specificChangelogPath = baseChangelogPath + changelogPathSpecification
-        if (commandLineInputFormat) {
-            if (!supportedChangeLogFormats.contains(commandLineInputFormat)) {
-                throw new IllegalArgumentException(commandLineInputFormat + " inputFormat is not supported")
-            }
-            TestConfig.instance.inputFormat = commandLineInputFormat
-        }
+
         Scope.getCurrentScope().getUI().sendMessage("Only " + TestConfig.instance.inputFormat
                 + " input files are taken into account for this test run")
 
@@ -37,19 +49,22 @@ class CompatibilityTestHelper {
         DatabaseConnectionUtil databaseConnectionUtil = new DatabaseConnectionUtil()
         for (DatabaseUnderTest databaseUnderTest : databaseConnectionUtil
                 .initializeDatabasesConnection(TestConfig.instance.getFilteredDatabasesUnderTest())) {
-            for (def changeLogEntry : FileUtils.resolveInputFilePaths(databaseUnderTest, specificChangelogPath, TestConfig.instance.inputFormat).entrySet()) {
-                if (!commandLineChangeObjectList || commandLineChangeObjectList.contains(changeLogEntry.key)) {
-                    inputList.add(TestInput.builder()
-                            .databaseName(databaseUnderTest.name)
-                            .url(databaseUnderTest.url)
-                            .dbSchema(databaseUnderTest.dbSchema)
-                            .username(databaseUnderTest.username)
-                            .password(databaseUnderTest.password)
-                            .version(databaseUnderTest.version)
-                            .change(changeLogEntry.key)
-                            .pathToChangeLogFile(changeLogEntry.value)
-                            .database(databaseUnderTest.database)
-                            .build())
+            for (String inputFormat: inputFormatList) {
+                for (def changeLogEntry : FileUtils.resolveInputFilePaths(databaseUnderTest, specificChangelogPath, inputFormat).entrySet()) {
+                    if (!commandLineChangeObjectList || commandLineChangeObjectList.contains(changeLogEntry.key)) {
+                        inputList.add(TestInput.builder()
+                                .databaseName(databaseUnderTest.name)
+                                .url(databaseUnderTest.url)
+                                .dbSchema(databaseUnderTest.dbSchema)
+                                .username(databaseUnderTest.username)
+                                .password(databaseUnderTest.password)
+                                .version(databaseUnderTest.version)
+                                .change(changeLogEntry.key)
+                                .inputFormat(inputFormat)
+                                .pathToChangeLogFile(changeLogEntry.value)
+                                .database(databaseUnderTest.database)
+                                .build())
+                    }
                 }
             }
         }
@@ -67,6 +82,7 @@ class CompatibilityTestHelper {
         String dbSchema
         String change
         String pathToChangeLogFile
+        String inputFormat
         Database database
     }
 }
