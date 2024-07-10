cfc9eda497206344ad6133aa87ccf44703d5c3a2
KushnirykOleh <kushnirykoleh@gmail.com>
2024-07-10 15:37:55 +0300

    trying showCollections

--- src/main/java/liquibase/ext/mongodb/database/MongoLiquibaseDatabaseUtil.java
+++ src/main/java/liquibase/ext/mongodb/database/MongoLiquibaseDatabaseUtil.java
@@ -1,27 +1,20 @@
 package liquibase.ext.mongodb.database;
 
 import com.mongodb.MongoException;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import liquibase.database.DatabaseConnection;
+import liquibase.Scope;
 import liquibase.exception.DatabaseException;
-import org.bson.BsonDocument;
-import org.bson.BsonInt64;
-import org.bson.conversions.Bson;
-
-import static liquibase.ext.mongodb.database.MongoLiquibaseDatabase.ADMIN_DATABASE_NAME;
+import liquibase.ext.mongodb.statement.ListCollectionNamesStatement;
 
 public class MongoLiquibaseDatabaseUtil {
     private MongoLiquibaseDatabaseUtil() {
     }
 
-    public static void sendPingSignal(DatabaseConnection connection) throws DatabaseException {
+    public static void checkCollectionsExist(MongoLiquibaseDatabase mongoLiquibaseDatabase) throws DatabaseException {
+        Scope.getCurrentScope().getLog(MongoLiquibaseDatabaseUtil.class).severe("--------- inside checkCollectionsExist ----------------------------");
+
         try {
-            if (connection instanceof MongoConnection) {
-                MongoClient mongoClient = ((MongoConnection) connection).getMongoClient();
-                Bson command = new BsonDocument("ping", new BsonInt64(1));
-                MongoDatabase adminDb = mongoClient.getDatabase(ADMIN_DATABASE_NAME);
-                adminDb.runCommand(command);
+            if (new ListCollectionNamesStatement().queryForList(mongoLiquibaseDatabase).isEmpty()) {
+                throw new DatabaseException("Database does not exist");
             }
         } catch (MongoException e) {
             throw new DatabaseException(e);
