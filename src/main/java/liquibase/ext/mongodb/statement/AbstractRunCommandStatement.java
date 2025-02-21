6a196db2556cc75e0479fc0777fefefc7b8fc9b7
Charles Queiroz <fcqueiroz@liquibase.com>
2025-02-21 12:26:16 +0000

    DAT-19473 - Solving the counting of affected rows.

--- src/main/java/liquibase/ext/mongodb/statement/AbstractRunCommandStatement.java
+++ src/main/java/liquibase/ext/mongodb/statement/AbstractRunCommandStatement.java
@@ -78,8 +78,7 @@ public abstract class AbstractRunCommandStatement extends AbstractMongoStatement
                 responseDocument.getDouble(OK);
         final List<Document> writeErrors = responseDocument.getList(WRITE_ERRORS, Document.class);
 
-        if (nonNull(ok) && !ok.equals(1.0d)
-                || nonNull(writeErrors) && !writeErrors.isEmpty()) {
+        if (!ok.equals(1.0d) || nonNull(writeErrors) && !writeErrors.isEmpty()) {
             throw new MongoException("Command failed. The full response is " + responseDocument.toJson());
         }
     }
